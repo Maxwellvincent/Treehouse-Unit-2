5515b4ec678a32f2a3c3a92892e280c2a6742cfd
Louis Maxwell <louismaxwell@knights.ucf.edu>
2019-09-12 22:33:31 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -59,23 +59,51 @@ window.onload=function() {
 ***/
 
    function appendPageLinks (list) {
+      // grabs total number of pages by taking amount in list / by amount of pages and then rounding the number up so that decimals wont occur.
       const totalPages = Math.ceil(list.length/ perPage);
+      // creating a div and unorder list to be appended to the main div.
       const div = document.createElement("DIV");
       const uL = document.createElement("UL");
-      const buttons = document.querySelector("pagination li")
-
+      // setting pagination class to div
       div.setAttribute("class","pagination");
       div.appendChild(uL);
       divPage.appendChild(div);
-
+      
+      // Creating a for loop to index through all pages
       for (var i = 1; i <= totalPages; i += 1) {
-         uL.innerHTML += "<li><a class='current-page' href='#'>"+ i +"</a></li>";
-         // buttons.addEventListener("click", function() {
-         //    alert("this was clciked");
-         // })
-      }
 
-      uL.firstChild.className ="active";
+         // creating li and a elements to each page of the pagination.
+         const listItem = document.createElement("li");
+         const anchor = document.createElement("a");
+
+         // creating text content to be placed within the anchor tag, along with setting attributes within the anchor element
+         anchor.textContent = i;
+         anchor.setAttribute('href','#');
+
+         // appending the a tage to the list item, and the list item to the pagination div.
+         listItem.appendChild(anchor);
+         uL.appendChild(listItem);
+
+          // Making sure that the first page is set to active. 
+          if(anchor.textContent === 1) {
+            anchor.className = "active";
+         }
+
+         // adding a click event listener to each a tag that get loops through
+         anchor.addEventListener("click", (e) => {
+            // Select all element that has the class of active
+            let activeElement = document.querySelectorAll('.active');
+            // Removing the active class thats placed on any element, and then setting active to the element that has been clicked. 
+
+            for (let i = 0; i < activeElement.length; i += 1){
+               activeElement[i].classList.remove('active');
+               e.target.className = 'active';
+            }
+            // Running the show page function, with what ever page has been clicked. 
+            showPage(showList, e.target.textContent);
+         });
+         
+      }
 
    }
 
